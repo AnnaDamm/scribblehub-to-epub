474a02ff3e0b516aea1c498e2bcb9694dbe999e3
Anna Damm <anna.damm87@googlemail.com>
2023-02-06 00:47:59 +0100

    fix error when parsing a relative date

--- src/ScribbleHub/book-metadata.js
+++ src/ScribbleHub/book-metadata.js
@@ -28,7 +28,7 @@ export class BookMetadata {
       },
       async () => { this.title = await page.$eval('meta[property="og:title"]', (element) => element.getAttribute('content')) },
       async () => { this.cover = new URL(await page.$eval('meta[property="og:image"]', (element) => element.getAttribute('content'))) },
-      async () => { this.date = new Date(await page.$eval('span[title^="Last updated:"]', (element) => element.getAttribute('title').replace(/^Last updated: /, ''))) },
+      async () => { this.date = this.parseUpdatedDate(await page.$eval('span[title^="Last updated:"]', (element) => element.getAttribute('title').replace(/^Last updated: /, ''))) },
       async () => { this.description = await page.$eval('meta[property="og:description"]', (element) => element.getAttribute('content')) },
       async () => {
         await page.$$eval('[class^="ad_"]', (nodes) => nodes.forEach((node) => node.remove()))
@@ -40,5 +40,29 @@ export class BookMetadata {
       async () => { this.publisher = await page.$eval('meta[property="og:site_name"]', (element) => element.getAttribute('content')) }
     ])
     eventEmitter.emit(bookMetadataLoaded, new BookMetadataLoadedEvent(this))
+  }
+
+  /**
+   * @param {string} dateString
+   * @return {Date|null}
+   */
+  parseUpdatedDate (dateString) {
+    let date = new Date(dateString)
+    if (date.toString() !== 'Invalid Date') {
+      return date
+    }
+    date = new Date()
+    let matches = dateString.match(/(?<hours>\d+) hours ago/)
+    if (matches !== null) {
+      date.setHours(date.getHours() - parseInt(matches.groups.hours, 10))
+      return date
+    }
+    matches = dateString.match(/(?<minutes>\d+) minutes ago/)
+    if (matches !== null) {
+      date.setMinutes(date.setMinutes() - parseInt(matches.groups.minutes, 10))
+      return date
+    }
+
+    return date
   }
 }
